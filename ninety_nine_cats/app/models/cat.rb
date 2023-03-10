40c85da1dde896f3a27b6de75dc3ac4ac803a5cc
Taisia Karaseva <taisiat@gmail.com>
2023-03-10 15:10:26 -0800

    Add cat model validations

--- ninety_nine_cats/app/models/cat.rb
+++ ninety_nine_cats/app/models/cat.rb
@@ -11,16 +11,26 @@
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
 #
+
+require 'action_view'
+
 class Cat < ApplicationRecord
+    include ActionView::Helpers::DateHelper
+    
     validates :birth_date, :color, :name, :sex, presence: true
     CAT_COLORS = ["black", "white", "orange", "gold", "red", "blue"]
     validates :color, inclusion: { in: CAT_COLORS, message: "%{value} is not a valid color"}
     validates :sex, inclusion: { in: %w(F M), message: "%{value} is not a valid sex"}
+    validate :birth_date_cannot_be_future 
 
     def birth_date_cannot_be_future
         if birth_date > Date.today
             errors.add(:birthday_error, "Cat can not be born in the future")
         end
+    end
+
+    def age
+        time_ago_in_words(birth_date)
     end
 
 end
