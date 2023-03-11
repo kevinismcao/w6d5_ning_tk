028ff7b506ccf2f8f9c001ff01a6e0cc21127d4d
Taisia Karaseva <taisiat@gmail.com>
2023-03-10 16:09:18 -0800

    Add cat controller and seeds

--- ninety_nine_cats/app/controllers/cats_controller.rb
+++ ninety_nine_cats/app/controllers/cats_controller.rb
@@ -21,12 +21,22 @@ class CatsController < ApplicationController
     end
 
     def edit
+        @cat = Cat.find_by(id: params[:id])
+        render :edit
     end
 
     def new
+        @cat = Cat.new
+        render :new
     end
 
     def update
+        @cat = Cat.find(params[:id])
+        if @cat&.update(cat_params)
+            redirect_to cat_url(@cat)
+        else
+            render json: @cat.errors.full_messages, status: 422
+        end
     end
 
     private
--- ninety_nine_cats/db/seeds.rb
+++ ninety_nine_cats/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Cat.destroy_all
+
+Cat.create(birth_date: "03/03/2002", color: "black", name: "bob", sex: "M", description: "gjnkskj")
+Cat.create(birth_date: "04/04/2015", color: "white", name: "mittens", sex: "F", description: "dfgh")
+Cat.create(birth_date: "05/06/2022", color: "red", name: "marley", sex: "F", description: "jyr")
+Cat.create(birth_date: "01/03/2021", color: "orange", name: "flippers", sex: "M", description: "hk")
+Cat.create(birth_date: "12/06/2008", color: "white", name: "tod", sex: "M")
